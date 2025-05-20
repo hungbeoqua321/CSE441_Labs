3231cbfd5e43fc048c014cd118e1b585590ea4f1
hungbeoqua321 <2151060248@e.tlu.edu.vn>
2025-05-21 00:21:48 +0700

    done lab07

---
+++ lab07/app/src/main/java/com/example/lab07/activity_child.java
@@ -0,0 +1,30 @@
+package com.example.lab07;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class activity_child extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_child);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainActivity.class));
+        });
+    }
+}
